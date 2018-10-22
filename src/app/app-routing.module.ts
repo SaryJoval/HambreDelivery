b3b2e3c6fd04c45226a8dace0dd22e39d0cc4d39
s.arenas.valenzuela <s.arenas.valenzuela@accenture.com>
2018-10-22 13:32:35 -0300

    adición navbar y rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,17 @@ import {HomePageComponent} from './componentes/home-page/home-page.component';
 import {LoginPageComponent} from './componentes/login-page/login-page.component';
 import {PrivadoPageComponent} from './componentes/privado-page/privado-page.component';
 import {NotFoundPageComponent} from './componentes/not-found-page/not-found-page.component';
-
-const routes: Routes = [];
+import {RegisterComponent} from './componentes/register/register.component';
+import {PedidoComponent} from './componentes/pedido/pedido.component';
+  
+const routes: Routes = [
+  {path:'', component: HomePageComponent},
+  {path:'login', component: LoginPageComponent},
+  {path:'privado', component: PrivadoPageComponent},
+  {path:'register', component: RegisterComponent},
+  {path:'pedido', component: PedidoComponent},
+  {path:'**', component:NotFoundPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
