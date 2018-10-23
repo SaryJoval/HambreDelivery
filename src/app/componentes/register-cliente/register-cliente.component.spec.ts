b76bf740fb17a672d24870232845b387439a4f46
s.arenas.valenzuela <s.arenas.valenzuela@accenture.com>
2018-10-23 12:18:54 -0300

    correcccion home, adicion comuna y ciudad en registro nuevo cliente, input y correcciones en pedido y correcion en registro usuario

---
+++ src/app/componentes/register-cliente/register-cliente.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { RegisterClienteComponent } from './register-cliente.component';
+
+describe('RegisterClienteComponent', () => {
+  let component: RegisterClienteComponent;
+  let fixture: ComponentFixture<RegisterClienteComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ RegisterClienteComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(RegisterClienteComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
