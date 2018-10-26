35c5b52881b6db1bcf639ad7937c16f03937766f
dani <dgajardoi2@hotmail.com>
2018-10-26 15:22:51 -0300

    Se modifico la clase usuario para que corresponda con los datos de usuarioDTO, se logro conectar con el microservicio.

--- src/app/classes/usuario.ts
+++ src/app/classes/usuario.ts
@@ -1,9 +1,9 @@
 export class Usuario {
 
-        public usuId: number;
-        public tusUsuId: number; //id de tabla tipo_usuario
-        public usuPas: String;
-        public usuNom: String;
-
+        public adminId: number;
+        public passwordAdmin: String;
+        public tipoUser: number; //id de tabla tipo_usuario
+        public nombreUser: String;
+        public password: String;
 
 }
--- src/app/servicios/user-service.service.ts
+++ src/app/servicios/user-service.service.ts
@@ -7,14 +7,16 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable()
 export class UserServiceService {
-  private usuarioUrl = 'http://localhost:9006/usuario';  // URL to web API
+  private usuarioUrl = 'http://localhost:9006/usuario/crear';  // URL to web API
   private headers = new HttpHeaders({'Content-Type': 'application/json'});
 
   constructor(private http: HttpClient) { }
 
   create(customer: Usuario): Promise<Usuario> {
-    customer.tusUsuId = 1;
-    customer.usuId = 3;
+    customer.adminId = 1;
+    customer.passwordAdmin = '1234';
+    customer.tipoUser = 1;
+
     return this.http
       .post(this.usuarioUrl, JSON.stringify(customer), {headers : this.headers, observe: 'response'})
       .toPromise()
